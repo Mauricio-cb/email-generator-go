c15f51466372c850945a23db369c078cf6ecb153
Mauricio <mbispo1202@gmail.com>
2023-02-19 15:32:35 +0000

    Mexendo no css e arrumando a função de gerar email

---
+++ Encrypt/Encrypt.go
@@ -0,0 +1,86 @@
+package Encrypt
+
+import (
+	
+	"crypto/sha1"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func EncryptPassword(text string) string{
+	hash := sha1.New()
+    hash.Write([]byte(text))
+
+	hashString := fmt.Sprintf("%x",  hash.Sum(nil))
+	
+
+	return hashString
+}
+
+func RandomEmail() string {
+
+	email := nameList() + secondArgumentToEmail() + "@gmail.com"
+
+	return email
+}
+
+func nameList() string {
+	names := []string{
+		"Miguel",
+		"Davi",
+		"Gabriel",
+		"Arthur",
+		"Lucas",
+		"Matheus",
+		"Pedro",
+		"Guilherme",
+		"Gustavo",
+		"Rafael",
+		"Felipe",
+		"Bernardo",
+		"Enzo",
+		"Nicolas",
+		"JoãoPedro",
+		"PedroHenrique",
+		"Cauã",
+		"Vitor",
+		"Eduardo",
+		"Daniel",
+		"Henrique",
+		"Murilo",
+		"Vinicius",
+		"Samuel",
+		"Pietro",
+		"João Vitor",
+		"Leonardo",
+		"Caio",
+		"Heitor",
+	}
+	rand.Seed(time.Now().UnixNano())
+
+	name := names[rand.Intn(len(names))]
+
+	return name
+}
+
+func secondArgumentToEmail() string {
+	args := []string{
+		"javeiro",
+		"jogador",
+		"odeiaPhp",
+		"javascriptAoContrario",
+		"jogadordelol",
+		"vascaino",
+		"flamenguista",
+		"rapzeiro",
+		"fantantasma",
+		"gotico",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	arg := args[rand.Intn(len(args))]
+
+	return arg
+}
--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	// "crypto/sha256"
-	// "fmt"
+	"html/template"
+	"log"
 	"net/http"
+
+	"github.com/Mauricio-cb/email-generator-go/Encrypt"
 )
 
+var tmpl *template.Template
 
-func main(){
-	dir := http.Dir("./html")
+type Data struct {
+	Email    string
+	Password string
+}
 
-	fs := http.FileServer(dir)
+func emailGeneratorHandler(w http.ResponseWriter, r *http.Request) {
+	data := Data{
+		Email:    Encrypt.RandomEmail(),
+		Password: Encrypt.EncryptPassword("daz"),
+	}
 
+	tmpl.Execute(w, data)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	tmpl = template.Must(template.ParseFiles("./static/html/email.html"))
+	fs := http.FileServer(http.Dir("./static/"))
 	
-	// teste := sha256.New()
-	// teste.Write([]byte("hello"))
-	// fmt.Printf("%x", teste.Sum(nil))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.Handle("/", fs)
+	mux.HandleFunc("/", emailGeneratorHandler)
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
+
 
 }
